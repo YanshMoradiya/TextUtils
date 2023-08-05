53686a3fa14fed3cc474222d3fed3b2936c22b9c
Yansh Moradiya <124123284+YanshMoradiya@users.noreply.github.com>
2023-08-05 22:13:58 +0530

    Update navbar.js

--- src/componants/navbar.js
+++ src/componants/navbar.js
@@ -1,61 +1,21 @@
-// import React from 'react'
-// import PropTypes from 'prop-types'
-// // import { Link } from "react-router-dom";
-// export default function Navbar(propes) {
-//     return (
-//         <>
-//             <nav className={`navbar navbar-expand-lg navbar-${propes.mode} bg-${propes.mode}`}>
-//                 <a className="navbar-brand" href="#">{propes.title}</a>
-//                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-//                     <span className="navbar-toggler-icon"></span>
-//                 </button>
-//                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-//                     <ul className="navbar-nav mr-auto">
-//                         <li className="nav-item active">
-//                             <a className="nav-link" href="#">Home </a>
-//                         </li>
-//                         <li className="nav-item">
-//                             <a className="nav-link" href="#">{propes.aboutText}</a>
-//                         </li>
-//                     </ul>
-//                     <div class="form-check form-switch">
-//                         <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={propes.toggleMode} />
-//                         <label class={`form-check-label text-${propes.mode === 'light' ? 'dark' : 'light'}`} for="flexSwitchCheckDefault">Enable Dark Mode</label>
-//                     </div>
-//                 </div>
-//             </nav >
-//         </>
-//     );
-// }
-
-// Navbar.propTypes = {
-//     title: PropTypes.string,
-//     aboutText: PropTypes.string
-// }
-
-// Navbar.defaultProps = {
-//     title: 'TextUtils',
-//     aboutText: 'about'
-// }
-
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom';
+// import { Link } from "react-router-dom";
 export default function Navbar(propes) {
     return (
         <>
             <nav className={`navbar navbar-expand-lg navbar-${propes.mode} bg-${propes.mode}`}>
-                <Link className="navbar-brand" to="/">{propes.title}</Link>
+                <a className="navbar-brand" href="#">{propes.title}</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <Link className="nav-link" to="/">Home </Link>
+                            <a className="nav-link" href="#">Home </a>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/about">{propes.aboutText}</Link>
+                            <a className="nav-link" href="#">{propes.aboutText}</a>
                         </li>
                     </ul>
                     <div class="form-check form-switch">
@@ -77,3 +37,5 @@ Navbar.defaultProps = {
     title: 'TextUtils',
     aboutText: 'about'
 }
+
+
